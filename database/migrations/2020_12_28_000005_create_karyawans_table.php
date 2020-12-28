18298980f70892d276ffae00316f08f4d51c4c2e
suhendar99 <xacepz00@gmail.com>
2020-12-28 09:40:40 +0700

    make migration for users

---
+++ database/migrations/2020_12_28_000005_create_karyawans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKaryawansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('karyawans', function (Blueprint $table) {
+            $table->id();
+            $table->string('jabatan');
+            $table->enum('gol_darah',['A','B','AB','O','Lainnya']);
+            $table->enum('jenis_kelamin',['Pria','Wanita']);
+            $table->enum('status_perkawinan',['Belum Kawin','Sudah']);
+            $table->string('kewarganegaraan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('karyawans');
+    }
+}
