24d0630aab9da35e9f80fceac870bbac8d8fdf81
apadilah30 <aguspadilah30@gmail.com>
2021-01-05 08:42:47 +0700

    start day

--- resources/views/layouts/dashboard/navbar.blade.php
+++ resources/views/layouts/dashboard/navbar.blade.php
@@ -11,38 +11,3 @@
         @endif 
     </div>
 </nav>
-<div class="container-fluid mt-3">
-    <div class=" row"> 
-        <div class=" col-12"> 
-            <div class=" row"> 
-                <div class=" col-md-8 col-sm-12"> 
-                    <div class=" row">  
-                        <div class="col-md-2 col-sm-4 text-md-left text-sm-center">
-                            <img src=" {{asset('images/logo-cdc.png')}}" class=" h-40 scale-down">
-                        </div>
-                        <div class=" col-md-10 col-sm-8 text-md-left text-sm-center"> 
-                            <h4 class="pl-2 py-1"> {{$pageTitle}}</h4>
-                        </div>
-                    </div>
-                </div>
-                @if(!isset($admin))
-                <div class=" col-md-4 col-sm-12">  
-                    <div class=" row"> 
-                        <div class=" col-md-12 col-sm-12"> 
-                            <div class=" form-group"> 
-                                <i class="material-icons md-24 icon-search">search</i>
-                                <input type=" text" name="search" class=" form-control rounded-40" placeholder=" Cari Barang ...">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                @endif
-            </div>
-        </div>
-    </div>  
-    <div class=" row"> 
-        <div class=" col-12"> 
-            @yield('content')
-        </div>
-    </div>   
-</div>
--- resources/views/layouts/dashboard/sidebar.blade.php
+++ resources/views/layouts/dashboard/sidebar.blade.php
@@ -1,118 +1,120 @@
-<nav>
-    <div class="sidebar-content shadow">
-        <div class="sidebar-header">
-            <center>
-                <img src="{{asset('images/logo-cdc.png')}}" class="rounded-circle mx-auto d-block avatar my-3">
-                <div class="btn-group dropright">
-                  <span type="button" class="text-white avatar-text" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }}</span>
-                  <div class="dropdown-menu">
-                    <a class="dropdown-item" href="#">Profil</a>
-                    <a class="dropdown-item" href="{{ route('logout') }}"
-                       onclick="event.preventDefault();
-                                     document.getElementById('logout-form').submit();">
-                        {{ __('Logout') }}
-                    </a>
+<div id="sidebar">
+    <nav>
+        <div class="sidebar-content shadow">
+            <div class="sidebar-header">
+                <center>
+                    <img src="{{asset('images/logo-cdc.png')}}" class="rounded-circle mx-auto d-block avatar my-3">
+                    <div class="btn-group dropright">
+                      <span type="button" class="text-white avatar-text" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }}</span>
+                      <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Profil</a>
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
 
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                        @csrf
-                    </form>
-                  </div>
-                </div>
-            </center>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                      </div>
+                    </div>
+                </center>
+            </div>
+            <ul class="list-unstyled components">
+                @if(isset($admin))
+                <li>
+                    <a href="#">
+                        <div class="sidebar card shadow mx-auto active d-flex justify-content-center">
+                            <i class="material-icons md-14">dashboard</i>
+                            <span class="sidebar span">Dashboard</span>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="sidebar card mx-auto d-flex justify-content-center">
+                            <i class="material-icons md-36">people</i>
+                            <span class="text-white f-9">Pengguna</span>
+                        </div>
+                    </a>
+                </li>
+                @elseif(isset($pemasok))
+                <li>
+                    <a href="#">
+                        <div class="sidebar card shadow mx-auto active d-flex justify-content-center">
+                            <i class="material-icons md-36">dashboard</i>
+                            <span class="sidebar span">Dashboard</span>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="sidebar card mx-auto d-flex justify-content-center">
+                            <i class="material-icons md-36">unarchive</i>
+                            <span class="text-white f-9">Tambah Barang</span>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="sidebar card mx-auto d-flex justify-content-center">
+                            <i class="material-icons md-36">local_shipping</i>
+                            <span class="text-white f-9">Barang Dikirim</span>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="sidebar card mx-auto d-flex justify-content-center">
+                            <i class="material-icons md-36">work</i>
+                            <span class="text-white f-9">Data Barang</span>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="sidebar card mx-auto d-flex justify-content-center">
+                            <i class="material-icons md-36">archive</i>
+                            <span class="text-white f-9">Pesanan Masuk</span>
+                        </div>
+                    </a>
+                </li>
+                @elseif(isset($karyawan))
+                <li>
+                    <a href="#">
+                        <div class="sidebar card shadow mx-auto active d-flex justify-content-center">
+                            <i class="material-icons md-36">dashboard</i>
+                            <span class="sidebar span">Dashboard</span>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="sidebar card mx-auto d-flex justify-content-center">
+                            <i class="material-icons md-36">archive</i>
+                            <span class="text-white f-9">Barang Masuk</span>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="sidebar card mx-auto d-flex justify-content-center">
+                            <i class="material-icons md-36">work</i>
+                            <span class="text-white f-9">Data Barang</span>
+                        </div>
+                    </a>
+                </li>
+                @endif
+                <li>
+                    <a href="#">
+                        <div class="sidebar card mx-auto d-flex justify-content-center">
+                            <i class="material-icons md-36">settings</i>
+                            <span class="text-white f-9">Settings</span>
+                        </div>
+                    </a>
+                </li>
+            </ul>
         </div>
-        <ul class="list-unstyled components">
-            @if(isset($admin))
-            <li>
-                <a href="#">
-                    <div class="sidebar card shadow mx-auto active d-flex justify-content-center">
-                        <i class="material-icons md-36">dashboard</i>
-                        <span class="sidebar span">Dashboard</span>
-                    </div>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <div class="sidebar card mx-auto d-flex justify-content-center">
-                        <i class="material-icons md-36">people</i>
-                        <span class="text-white f-9">Pengguna</span>
-                    </div>
-                </a>
-            </li>
-            @elseif(isset($pemasok))
-            <li>
-                <a href="#">
-                    <div class="sidebar card shadow mx-auto active d-flex justify-content-center">
-                        <i class="material-icons md-36">dashboard</i>
-                        <span class="sidebar span">Dashboard</span>
-                    </div>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <div class="sidebar card mx-auto d-flex justify-content-center">
-                        <i class="material-icons md-36">unarchive</i>
-                        <span class="text-white f-9">Tambah Barang</span>
-                    </div>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <div class="sidebar card mx-auto d-flex justify-content-center">
-                        <i class="material-icons md-36">local_shipping</i>
-                        <span class="text-white f-9">Barang Dikirim</span>
-                    </div>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <div class="sidebar card mx-auto d-flex justify-content-center">
-                        <i class="material-icons md-36">work</i>
-                        <span class="text-white f-9">Data Barang</span>
-                    </div>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <div class="sidebar card mx-auto d-flex justify-content-center">
-                        <i class="material-icons md-36">archive</i>
-                        <span class="text-white f-9">Pesanan Masuk</span>
-                    </div>
-                </a>
-            </li>
-            @elseif(isset($karyawan))
-            <li>
-                <a href="#">
-                    <div class="sidebar card shadow mx-auto active d-flex justify-content-center">
-                        <i class="material-icons md-36">dashboard</i>
-                        <span class="sidebar span">Dashboard</span>
-                    </div>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <div class="sidebar card mx-auto d-flex justify-content-center">
-                        <i class="material-icons md-36">archive</i>
-                        <span class="text-white f-9">Barang Masuk</span>
-                    </div>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <div class="sidebar card mx-auto d-flex justify-content-center">
-                        <i class="material-icons md-36">work</i>
-                        <span class="text-white f-9">Data Barang</span>
-                    </div>
-                </a>
-            </li>
-            @endif
-            <li>
-                <a href="#">
-                    <div class="sidebar card mx-auto d-flex justify-content-center">
-                        <i class="material-icons md-36">settings</i>
-                        <span class="text-white f-9">Settings</span>
-                    </div>
-                </a>
-            </li>
-        </ul>
-    </div>
-</nav>
+    </nav>
+</div>
