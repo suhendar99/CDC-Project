ec77304990cb96e954a6cf28fdfa32a74a879f82
agus padilah <aguspadilah30@gmail.com>
2021-01-08 08:13:54 +0700

    start day

--- resources/views/app/shop/index.blade.php
+++ resources/views/app/shop/index.blade.php
@@ -62,38 +62,49 @@
 					  </a>
 					</div>
 				</div>
-			    <div class="col-md-12 valign-center">
-			      <i class="material-icons md-36 text-my-danger mr-1">local_fire_department</i><span style="font-size: 18px"> Sembako Populer</span>
-			    </div>
-			    <div class="col-12">
-			        <div class="MultiCarousel" data-items="1,3,5,6" data-slide="1" id="MultiCarousel"  data-interval="1000">
-			            <div class="MultiCarousel-inner">
-			                @foreach($data as $d)
-			                <div class="item px-1">
-			                    <div class="card shadow">
-			                        <img class="card-img-top" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_176acb1444e%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_176acb1444e%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2299.1171875%22%20y%3D%2296.3%22%3EImage%20cap%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Card image cap">
-			                        <div class="card-body text-left">
-			                            <span class="carousel-card-title">Beras Super {{$d}}</span><br>
-			                            <span class="carousel-card-subtitle">Tersedia {{$d}} Ton</span><br>
-			                            {{-- <div class="row border-top pt-1">
-			                                <div class="col-12 d-flex justify-content-center">
-			                                    <a class="btn bg-transparent" href="#" title="Edit" tooltip="Edit">
-			                                        <i class="material-icons text-my-warning md-18 ">edit</i>
-			                                    </a>
-			                                    <a class="btn bg-transparent" href="#" title="Hapus" tooltip="Hapus">
-			                                        <i class="material-icons text-my-danger md-18 ">delete</i>
-			                                    </a>
-			                                </div>
-			                            </div> --}}
-			                        </div>
-			                    </div>
-			                </div>
-			                @endforeach
-			            </div>
-			            <button class="btn leftLst"><</button>
-			            <button class="btn rightLst">></button>
-			        </div>
-			    </div>
+			</div>
+			{{-- <div class="row">
+		    <div class="col-md-12 valign-center">
+		      <i class="material-icons md-36 text-my-danger mr-1">local_fire_department</i><span style="font-size: 18px"> Sembako Populer</span>
+		    </div>
+		    <div class="col-12">
+		        <div class="MultiCarousel" data-items="1,3,5,6" data-slide="1" id="MultiCarousel"  data-interval="1000">
+		            <div class="MultiCarousel-inner">
+		                @foreach($data as $d)
+		                <div class="item px-1">
+		                    <div class="card shadow">
+		                        <img class="card-img-top" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_176acb1444e%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_176acb1444e%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2299.1171875%22%20y%3D%2296.3%22%3EImage%20cap%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Card image cap">
+		                        <div class="card-body text-left">
+		                            <span class="carousel-card-title">Beras Super {{$d}}</span><br>
+		                            <span class="carousel-card-subtitle">Tersedia {{$d}} Ton</span><br>
+		                        </div>
+		                    </div>
+		                </div>
+		                @endforeach
+		            </div>
+		            <button class="btn leftLst"><</button>
+		            <button class="btn rightLst">></button>
+		        </div>
+		    </div>
+			</div> --}}
+			<div class="row">
+				<div class="col-md-3">
+					<div class="card item-card">
+						<img src="https://cf.shopee.co.id/file/08744f5b0e1ab3e2d537df5bbf5b2bb4">
+						<div class="card-body">
+							<div class="row">
+								<div class="col-12">
+									Beras Super, 1Kg
+								</div>
+								<div class="col-12">
+									Stok : 20 Item	
+								</div>
+							</div>
+							<button type="button" class="btn btn-primary btn-sm mx-1">Keranjang</button>
+							<button type="button" class="btn btn-warning btn-sm mx-1">Beli</button>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
