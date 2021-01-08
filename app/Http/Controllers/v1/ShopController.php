0f55f83de1365979a3d52ab076feddc8bab3771c
agus padilah <aguspadilah30@gmail.com>
2021-01-09 00:22:18 +0700

    fix shop

--- app/Http/Controllers/v1/ShopController.php
+++ app/Http/Controllers/v1/ShopController.php
@@ -5,17 +5,20 @@ namespace App\Http\Controllers\v1;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Kategori;
+use App\Models\Barang;
 
 class ShopController extends Controller
 {
 	public function __construct()
 	{
 		$this->category = new Kategori;
+		$this->barang = new Barang;
 	}
 	public function index()
 	{
 		$category = $this->category->getAll();
-		dd($category);
-		return view('app.shop.index', compact($category));
+		$barang = $this->barang->limit(20)->get();
+		// dd($category);
+		return view('app.shop.index', compact('category','barang'));
 	}
 }
