eedbb71bfc2268ecfc51f8be4c08a3ac8be089c3
suhendar99 <xacepz00@gmail.com>
2021-01-19 15:19:12 +0700

    model

---
+++ database/migrations/2021_01_19_150910_create_pemesanans_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePemesanansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pemesanans', function (Blueprint $table) {
+            $table->id();
+            $table->string('barang_kode');
+            $table->foreign('barang_kode')->references('kode_barang')->on('barangs')->onDelete('cascade');
+            $table->integer('jumlah_barang');
+            $table->string('nama_pemesan');
+            $table->string('alamat_pemesan');
+            $table->date('tanggal_pemesanan');
+            $table->boolean('metode_pembayaran');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pemesanans');
+    }
+}
