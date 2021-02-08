d19c7eb9b5d6a749b8ae0ceb9a36f49ceb2ccc8f
agus padilah <aguspadilah30@gmail.com>
2021-02-08 20:42:56 +0700

    push table storgae in out retail

--- database/migrations/2021_01_05_124315_create_stock_barangs_table.php
+++ database/migrations/2021_01_05_124315_create_stock_barangs_table.php
@@ -16,8 +16,9 @@ class CreateStockBarangsTable extends Migration
         Schema::create('stock_barangs', function (Blueprint $table) {
             $table->id();
             $table->foreignId('gudang_id')->constrained('gudangs')->onDelete('cascade');
-            $table->string('barang_kode');
-            $table->foreign('barang_kode')->references('kode_barang')->on('barangs')->onDelete('cascade');
+            $table->foreignId('barang_bulky_id')->constrained('storage_bulkies')->onDelete('cascade');
+            $table->string('nama_barang', 50);
+            $table->text('foto_barang');
             $table->float('jumlah', 11, 2);
             $table->string('satuan', 50);
             $table->bigInteger('harga_barang')->nullable();
