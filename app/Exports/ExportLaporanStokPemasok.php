0e8ce287f3c14128f049ba10ee4e44df052c8159
suhendar99 <xacepz00@gmail.com>
2021-02-15 14:37:00 +0700

    new update laporan

---
+++ app/Exports/ExportLaporanStokPemasok.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\Barang;
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+
+class ExportLaporanStokPemasok implements FromView,ShouldAutoSize
+{
+    use Exportable;
+
+    public $awal;
+    public $akhir;
+    public $bulan;
+    public $month;
+    public $hii;
+
+    function __construct($awal,$akhir,$bulan,$month,$hii){
+        $this->awal = $awal;
+        $this->akhir = $akhir;
+        $this->bulan = $bulan;
+        $this->month = $month;
+        $this->hii = $hii;
+        $this->path = 'app.laporan.pemasok.stok.';
+    }
+    public function view(): View
+    {
+        if ($this->bulan != null && $this->month) {
+            $data = Barang::with('pemasok')->whereRaw('MONTH(created_at) = '.$this->hii)->get();
+        } elseif ($this->awal != null && $this->akhir != null) {
+            $data = Barang::whereBetween('created_at',[$this->awal, $this->akhir])->get();
+        }
+        return view($this->path.'excel', compact('data'));
+
+    }
+}
--- app/Http/Controllers/v1/LaporanPemasokController.php
+++ app/Http/Controllers/v1/LaporanPemasokController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\v1;
 
 use App\Exports\ExportLaporanPenjualanPemasok;
+use App\Exports\ExportLaporanStokPemasok;
 use App\Http\Controllers\Controller;
+use App\Models\Barang;
 use App\Models\StorageKeluarPemasok;
 use DateTime;
 use PDF;
@@ -99,6 +101,90 @@ class LaporanPemasokController extends Controller
                 $input = $request->all();
                 set_time_limit(99999);
                 return (new ExportLaporanPenjualanPemasok($awal, $akhir,$bulan,$month,$hii))->download('Laporan-'.$akhir.'.xlsx');
+        }
+    }
+    public function showLaporanStok()
+    {
+        return view($this->pathStok.'index');
+    }
+    public function LaporanStokPdf(Request $request)
+    {
+        if ($request->month != null && $request->has('month')) {
+            $v = Validator::make($request->all(),[
+                'month' => 'required'
+            ]);
+        } elseif ($request->awal != null && $request->akhir != null) {
+            $v = Validator::make($request->all(),[
+                'awal' => 'required',
+                'akhir' => 'required'
+            ]);
+        }
+
+        if ($v->fails()) {
+            return back()->withErrors($v)->withInput();
+        } else {
+            $awal = $request->awal;
+            $akhir = $request->akhir;
+            $month = $request->month;
+            if ($request->month != null && $request->has('month')) {
+                if ($request->month == null) {
+                    return back()->with('error','Mohon Pilih Bulan !');
+                }
+                $dateObj = DateTime::createFromFormat('!m',$month);
+                $sumber = 'Bulan '.$dateObj->format('F');
+                $bulan = $request->input('month');
+                $data = Barang::with('pemasok')->whereRaw('MONTH(created_at) = '.$bulan)->get();
+
+                $pdf = PDF::loadview($this->pathStok.'pdf',compact('data','awal','akhir','sumber','month'))->setPaper('DEFAULT_PDF_PAPER_SIZE', 'landscape')->setWarnings(false);
+                set_time_limit('99999');
+                return $pdf->stream('Laporan'.$sumber.$akhir.'.pdf');
+                return view($this->pathStok.'pdf',compact('data','awal','akhir','sumber','month'));
+
+            } elseif ($awal != null && $akhir != null) {
+                if ($akhir < $awal) {
+                    return back()->with('failed','Mohon Periksa Tanggal Anda !');
+                }
+                $data = Barang::with('pemasok')->whereBetween('created_at',[$awal, $akhir])->latest()->get();
+
+                $pdf = PDF::loadview($this->pathStok.'pdf',compact('data','awal','akhir','month'))->setPaper('DEFAULT_PDF_PAPER_SIZE', 'landscape')->setWarnings(false);
+                set_time_limit('99999');
+                return $pdf->stream('Laporan'.$akhir.'.pdf');
+                return view($this->pathStok.'pdf',compact('data','awal','akhir','month'));
+            }
+            $null  = Barang::all();
+            if ($null == null) {
+                return back()->with('error','Tidak ada data !');
+            }
+        }
+    }
+    public function LaporanStokExcel(Request $request)
+    {
+        if ($request->month != null && $request->has('month')) {
+            $v = Validator::make($request->all(),[
+                'month' => 'required'
+            ]);
+        } elseif ($request->awal != null && $request->akhir != null) {
+            $v = Validator::make($request->all(),[
+                'awal' => 'required',
+                'akhir' => 'required'
+            ]);
+        }
+
+        if ($v->fails()) {
+            return back()->withErrors($v)->withInput();
+        } else {
+            $awal = $request->awal;
+            $akhir = $request->akhir;
+            $bulan = $request->month;
+            $month = $request->has('month');
+            $hii = $request->input('month');
+            $data = Barang::all();
+            if($data->count() < 1){
+                return back()->with('failed','Data Kosong!');
+            }
+                $input = $request->all();
+                set_time_limit(99999);
+                return (new ExportLaporanStokPemasok($awal, $akhir,$bulan,$month,$hii))->download('Laporan-'.$akhir.'.xlsx');
         }
     }
 }
