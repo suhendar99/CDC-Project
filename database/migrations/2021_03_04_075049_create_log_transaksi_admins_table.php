7d37d32469ad1eb2bac507fe1e33843a12398e7f
faliq_14 <faliq.sawala.space>
2021-03-04 09:20:56 +0700

    add log pemesanan dashboard admin

---
+++ database/migrations/2021_03_04_075049_create_log_transaksi_admins_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLogTransaksiAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('log_transaksi_admins', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('time', 0);
+            $table->string('transaksi');
+            $table->string('penjual');
+            $table->string('pembeli');
+            $table->string('barang');
+            $table->bigInteger('jumlah');
+            $table->string('satuan');
+            $table->string('harga');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('log_transaksi_admins');
+    }
+}
